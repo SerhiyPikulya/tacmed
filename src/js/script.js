56b8e65076ac351574877263705bd558407f0b75
SerhiyPikulya <serhiy.pikulya@gmail.com>
2023-04-26 11:34:02 +0300

    slider

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+            // {
+            //     breakpoint: 768,
+            //     settings: {
+            //         arrows: false,
+            //         centerMode: true,
+            //         centerPadding: '40px',
+            //         slidesToShow: 1
+            //    }
+            // },
+        //     {
+        //       breakpoint: 480,
+        //       settings: {
+        //         arrows: false,
+        //         centerMode: true,
+        //         centerPadding: '40px',
+        //         slidesToShow: 1
+        //       }
+        //     }
+        ]
+        //autoplay: true,
+        //autoplaySpeed: 2000
+        //fade: true,
+        //cssEase: 'linear'
+    });
+});
